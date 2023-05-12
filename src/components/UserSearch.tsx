61c7af56901c61330885189a42e883f9cf331d46
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2023-05-12 17:58:18 -0300

    Modo dark adicionado

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -81,11 +81,11 @@ const UserSearch: React.FC<UserSearchProps> = ({ onUserClick }) => {
         </div>
       </form>
 
-      {error && <p className="text-red-500 mt-2">{error}</p>}
+      {error && <p className="flex items-center justify-center text-red-500 mt-2">{error}</p>}
 
       {loading && (
         <div className="flex justify-center items-center mt-28">
-          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+          <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-white"></div>
         </div>
       )}
       
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          primary: '#1e293b',
+          secondary: '#3d4b64',
+          // Adicione outras cores escuras personalizadas aqui
+        },
+      },
+
+    },
   },
   plugins: [],
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
